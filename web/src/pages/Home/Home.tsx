03ca4b43b8a620f8e73ef07e9802795cd95e9e7e
bruno <bruno.costa@cinnecta.com>
2020-06-03 23:14:41 -0300

    Início da implementação da web

---
+++ web/src/pages/Home/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './style.css'
+
+const Home = () => {
+    return (
+        <div id="page-home">
+            <div className="content">
+                <header>Seu marketplace de coleta de resíduos</header>
+                <main>Ajudamos pessoas a encontrarem pontos de coletas de forma eficiente.</main>
+            </div>
+        </div>
+    )
+}
+
+export default Home
